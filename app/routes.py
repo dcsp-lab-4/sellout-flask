45481ac4b45d8e670175421a5f7045b9182544e6
David Lewis <david.wallin.lewis@gmail.com>
2019-04-10 01:47:28 -0500

    fixing the registration that i broke

--- app/routes.py
+++ app/routes.py
@@ -42,7 +42,12 @@ def register():
         return redirect(url_for('index'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(username=form.username.data, email=form.email.data, usertype=form.usertype.data)
+        user = User(username=form.username.data, 
+            email=form.email.data, 
+            phone=form.phone.data,
+            firstname=form.firstname.data,
+            lastname=form.lastname.data,
+            usertype=form.usertype.data)
         user.set_password(form.password.data)
         db.session.add(user)
         db.session.commit()
